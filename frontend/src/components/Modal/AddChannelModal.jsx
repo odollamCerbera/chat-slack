b4315cab99f7831156c722b3722a0439a1ca5dfc
Cerbera <bykovaem.klp@gmail.com>
2026-03-11 15:12:07 +0500

    Step 6: Add modal for rename channels

--- frontend/src/components/Modal/AddChannelModal.jsx
+++ frontend/src/components/Modal/AddChannelModal.jsx
@@ -1,13 +1,10 @@
 import { closeModal } from '@slices/modalSlice'
 import { selectChannelsNames } from '@store/selectors'
-import { createChannel } from '@thunks/channelThunk'
-import leoProfanity from '@utils/profanity'
-import { getChannelSchema } from '@utils/schema/channelSchema'
-import { Formik } from 'formik'
-import { Button, Form, Modal } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
-import FormField from '../Ui/FormField'
+import FormInModal from './FormInModal'
+
 
 const AddChannelModal = () => {
   const { t } = useTranslation()
@@ -21,45 +18,7 @@ const AddChannelModal = () => {
       <Modal.Header onHide={handleClose} closeButton>
         <Modal.Title>{t('channels.createChannel')}</Modal.Title>
       </Modal.Header>
-
-      <Formik
-        initialValues={{ name: '' }}
-        validationSchema={getChannelSchema(t, existingChannels)}
-        onSubmit={async (values, { setSubmitting }) => {
-          try {
-            const cleanName = leoProfanity.clean(values.name.trim())
-            await dispatch(createChannel(cleanName)).unwrap()
-            handleClose()
-          } finally {
-            setSubmitting(false)
-          }
-        }}
-      >
-        {({ handleSubmit, isSubmitting }) => (
-          <Form onSubmit={handleSubmit}>
-            <Modal.Body>
-              <Form.Group>
-                <FormField
-                  name='name'
-                  label={t('channels.channel')}
-                  type='text'
-                  autoFocus
-                />
-              </Form.Group>
-            </Modal.Body>
-
-            <Modal.Footer>
-              <Button variant='secondary' onClick={handleClose} disabled={isSubmitting}>
-                {t('channels.cancel')}
-              </Button>
-
-              <Button type='submit' variant='primary' disabled={isSubmitting}>
-                {t('channels.send')}
-              </Button>
-            </Modal.Footer>
-          </Form>
-        )}
-      </Formik>
+      <FormInModal />
     </>
   )
 }
--- frontend/src/components/Modal/RenameChannelModal.jsx
+++ frontend/src/components/Modal/RenameChannelModal.jsx
@@ -1,5 +1,25 @@
+import { closeModal } from '@slices/modalSlice'
+import { selectChannelsNames } from '@store/selectors'
+import { Modal } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import FormInModal from './FormInModal'
+
 const RenameChannelModal = () => {
-  // тут модалка для переименования
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const existingChannels = useSelector(selectChannelsNames)
+
+  const handleClose = () => dispatch(closeModal())
+
+  return (
+    <>
+      <Modal.Header onHide={handleClose} closeButton>
+        <Modal.Title>{t('channels.renameChannel')}</Modal.Title>
+      </Modal.Header>
+      <FormInModal />
+    </>
+  )
 }
 
 export default RenameChannelModal
