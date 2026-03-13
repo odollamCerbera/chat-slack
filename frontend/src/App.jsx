8b7c2961d238d513679189edbb434690f617a363
Cerbera <bykovaem.klp@gmail.com>
2026-03-13 11:58:04 +0500

    Step 11: Test Rollbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,13 @@ const App = () => {
   const dispatch = useDispatch()
   const isAuthenticated = useSelector(selectIsAuthenticated)
 
+  ///////////////////////
+  useEffect(() => {
+    console.log('🔑 Rollbar token exists:', !!import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN)
+    console.log('🌍 Environment:', import.meta.env.MODE)
+  }, [])
+  ///////////////////////
+
   useEffect(() => {
     if (!isAuthenticated) {
       socket.disconnect()
